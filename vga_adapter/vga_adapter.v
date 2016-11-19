0456e91927ee4a5c7d968a05a0e79d0f6b0c68e1
phophores <paulyf.wang@mail.utoronto.ca>
2016-11-19 17:44:17 -0500

    changed vga to 2 bits per color channel in vga adapter.v

--- vga_adapter/vga_adapter.v
+++ vga_adapter/vga_adapter.v
@@ -90,7 +90,7 @@ module vga_adapter(
 			VGA_SYNC,
 			VGA_CLK);
  
-	parameter BITS_PER_COLOUR_CHANNEL = 1;
+	parameter BITS_PER_COLOUR_CHANNEL = 2;
 	/* The number of bits per colour channel used to represent the colour of each pixel. A value
 	 * of 1 means that Red, Green and Blue colour channels will use 1 bit each to represent the intensity
 	 * of the respective colour channel. For BITS_PER_COLOUR_CHANNEL=1, the adapter can display 8 colours.
